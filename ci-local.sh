9af1ccbb3cde63201f0c755af4e77bb0c147fe88
Brett Tolbert <bretttolbert@gmail.com>
2026-09-20 19:46:47 -0500

    added track yaml files

--- ci-local.sh
+++ ci-local.sh
@@ -30,4 +30,4 @@ python -m pip install -e ./moongas-mediascripts-python
 python -m pip install -e ./moongas-mediatunes-svc-python-blacksheep
 
 echo "==> Run mediatest on moongas-collection"
-python -m mediatest --log-level=DEBUG -v -k "not test_album_dir_is_not_empty and not test_album_cover_exists" "$REPO_ROOT/mediatest-config.yml"
+python -m mediatest --log-level=DEBUG -v -k "not test_album_cover_exists" "$REPO_ROOT/mediatest-config.yml"
